cc1505ff21ef667a3e8b409dfda8a6eb49744358
Joe Buhr <jtb75@users.noreply.github.com>
2024-11-21 10:38:58 -0600

    Update backend_config.tf

--- backend_config.tf
+++ backend_config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3"{
-  bucket = ${{ secrets.BUCKET_TF_STATE }}
-  key = tf-state-124jk123
-  region = "us-east-2"
+  bucket = "tf-state-124jk123"
+  key = "terrraform.tfstate"
+  region = "us-east-1"
   }
 }
