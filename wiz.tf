ab3a0ad9316335cae84bf48ddc10fbf47bf802f7
jtb75 <joe.buhr@gmail.com>
2024-11-26 06:15:24 -0600

    Added event ingestion stack

--- wiz.tf
+++ wiz.tf
@@ -6,3 +6,26 @@ module "wiz" {
   eks-scanning       = true
   remote-arn         = "arn:aws:iam::197171649850:role/prod-us20-AssumeRoleDelegator"
 }
+
+module "aws_cloud_events" {
+  source = "https://downloads.wiz.io/customer-files/aws/wiz-aws-cloud-events-terraform-module.zip"
+
+  cloudtrail_arn        = aws_cloudtrail.cloudtrail.arn
+  cloudtrail_bucket_arn = aws_s3_bucket.cloudtrail_bucket.arn
+  #cloudtrail_kms_arn    = "<CLOUDTRAIL_KMS_ARN>"
+
+  use_existing_sns_topic       = true
+  sns_topic_arn                = aws_sns_topic.cloudtrail_topic.arn
+  sns_topic_encryption_enabled = true
+  sns_topic_encryption_key_arn = aws_kms_key.sns_key.arn
+
+  wiz_access_role_arn = module.wiz.role_arn
+}
+
+output "bucket_name" {
+    value = module.aws_cloud_events.bucket_name
+}
+
+output "bucket_account" {
+    value = module.aws_cloud_events.bucket_account
+}
